b3fc35044606dd466844ad41d8b4bddbe9cf1a0f
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-03-08 09:56:43 +0900

    feat: paginationコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import Pagination from './Pagination'
 import Album from './type'
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-
+      <Pagination />
     </div>
   )
 }
