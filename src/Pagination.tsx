ba72a2ee5f49463a6568614a457b21c8b9420810
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-03-16 09:52:06 +0900

    feat: 画像を6枚のみ表示するように変更

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AlbumList from "./AlbumList";
 import "./Pagination.css";
 import Album from "./type";
@@ -11,7 +11,16 @@ type Props = {
 const Pagination = (props: Props) => {
   const { albums } = props
 
-  return (<div className="albumWrapper"><AlbumList albums={albums} /></div>)
+  // 1ページあたり6要素表示
+  const itemsPerPage = 6
+
+  const [itemsOffset, setItemsOffset] = useState(0)
+
+  const endOffset = itemsOffset + itemsPerPage;
+
+  const currentAlbums = albums.slice(itemsOffset, endOffset)
+
+  return (<div className="albumWrapper"><AlbumList albums={albums} currentAlbums={currentAlbums} /></div>)
 }
 
 export default Pagination;
